8bf5854302e48c3b148bf1549e7edfa42d5d3670
Hendi Santika <hendisantika@gmail.com>
2021-11-25 17:50:57 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootPostgresqlFlywayJsobApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresqlFlywayJsobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgresqlFlywayJsobApplication.class, args);
+    }
+
+}
