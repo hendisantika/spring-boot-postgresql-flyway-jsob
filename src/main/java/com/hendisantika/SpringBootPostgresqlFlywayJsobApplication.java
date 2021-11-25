aea97907e4da259e830d8bca47c6cca4e19ce859
Hendi Santika <hendisantika@gmail.com>
2021-11-25 18:30:19 +0700

    #13 - Add OktaOAuth2WebSecurityConfigurerAdapter

--- src/main/java/com/hendisantika/SpringBootPostgresqlFlywayJsobApplication.java
+++ src/main/java/com/hendisantika/SpringBootPostgresqlFlywayJsobApplication.java
@@ -2,12 +2,28 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringBootPostgresqlFlywayJsobApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPostgresqlFlywayJsobApplication.class, args);
     }
 
+    @Configuration
+    static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                    .oauth2ResourceServer().jwt();
+        }
+    }
 }
